bdc8e6a9a61628592eea066ff6ee468a72ea55ca
spulido99 <spulido99@gmail.com>
2020-01-28 18:31:26 -0500

    create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+# 1. Instalar git
+
+# 2. Instalar herramientas de desarrollo
+
+# 2.1. Instalar Node.js
+
+# 2.2. Instalar visual studio code
+
+# 2.3 Instalar librerías
+
+# 3. Instalar herramientas de bases de datos
+
+# 3.1. Instalar motor de base de datos
+
+# 3.2. Instalar visualizador de base de datos
+
+# 4. Instalar herramientas para back
+
+# 4.1. Instalar librerías
+
+# 5. Bajar applicación base
